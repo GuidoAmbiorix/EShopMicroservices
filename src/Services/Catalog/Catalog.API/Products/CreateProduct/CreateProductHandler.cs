be995a8972fb36af6b92f1fb6f4aa7b718a15947
GuidoA_Amparo <GuidoA_Amparo@jce.do>
2024-05-27 15:51:59 -0400

    --Se agrego docker compose al proyecto

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,13 +1,11 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
+﻿
 namespace Catalog.API.Products.CreateProduct;
 
-public record CreateProductCommand(string Name,List<string> Category,string Description,string ImageFile,decimal Price)
-    :ICommand<CreateProductResult>;
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
@@ -26,8 +24,11 @@ internal class CreateProductHandler : ICommandHandler<CreateProductCommand, Crea
 
         //TODO
         //save to database
-        return new CreateProductResult(Guid.NewGuid());
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        //return result
+        return new CreateProductResult(product.Id);
 
     }
 }
- 
