a51cff74eabe2d7c0f88cea806bade810c71a3db
guidoa_amparo <guidoa_amparo@jce.do>
2024-07-11 20:26:36 -0400

    se estan haciendo los comandos y queries de ordering

--- src/Services/Ordering/Ordering.Infraestructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infraestructure/DependencyInjection.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Data;
+using Ordering.Infraestructure.Data;
 
 namespace Ordering.Infraestructure;
 public static class DependencyInjection
@@ -7,6 +10,18 @@ public static class DependencyInjection
     public static IServiceCollection AddInfraestructureServices(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("Database");
+
+        // Add services to the container
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+        services.AddDbContext<ApplicationDbContext>((sp, options) =>
+        {
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            options.UseSqlServer(connectionString);
+        });
+
+        services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
         return services;
     }
 }
